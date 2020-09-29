1e326eb30618007940a32b9cdac1a15c5549b24c
WANJIN <wanjin@megazone.com>
2020-09-29 13:03:01 +0900

    rollup config, package.json and other config files added

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires,global-require */
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
-import vue from 'rollup-plugin-vue2'; // Handle .vue SFC files
+import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
+import VuePlugin from 'rollup-plugin-vue'; // Handle .vue SFC files
 import css from 'rollup-plugin-css-only';
 import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
 import typescript from 'rollup-plugin-typescript2';
 import jsx from 'acorn-jsx';
 
 export default {
-    input: 'src/wrapper.ts',
+    input: 'src/wrapper.js',
     output: [
         {
             format: 'cjs',
@@ -29,9 +30,9 @@ export default {
     plugins: [
         typescript({
             typescript: require('typescript'),
-            objectHashIgnoreUnknownHack: true,
+            // tsconfig: 'tsconfig.json',
         }),
-        vue({
+        VuePlugin({
             preprocessStyles: true,
             postcssPlugins: [
                 require('postcss-easy-import')({
@@ -42,14 +43,16 @@ export default {
                 require('postcss-mixins'),
                 require('postcss-conditionals'),
                 require('postcss-nested'),
-                require('postcss-simple-vars')({
-                }),
+                require('postcss-simple-vars')(),
                 require('autoprefixer'),
                 require('postcss-preset-env')({ stage: 3 }),
             ],
         }),
         css(),
-        buble(), // Transpile to ES5
+        buble({
+            transforms: { forOf: false, generator: false },
+            objectAssign: 'Object.assign',
+        }), // Transpile to ES5
         resolve({ browser: true, jsnext: true, main: true }),
         commonjs(),
     ],
